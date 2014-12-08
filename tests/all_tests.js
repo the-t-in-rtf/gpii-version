42002f1b4110ad84052a7c9d5b4418fac9fba5f4
Tony <tony@raisingthefloor.org>
2014-12-08 17:33:48 +0100

    Left working notes for tomorrow's test writing.

--- tests/all_tests.js
+++ tests/all_tests.js
@@ -1,6 +1,17 @@
-// TODO:  Make this a real list of tests
+// There is no setup function, so we have to manually remove our test data before beginning.
+var version = require("../")({"repoDir": "/tmp/version-test"});
 
-var version = require("../")({});
+// There is no setup function in jqUnit, so we have to wait manually until the repository is initialized before we continue
+setTimeout(runTests, 3000);
 
-// TODO:  Make sure the repository is initialized before we continue
-setTimeout(function() { version.store("1234", {"foo": "bar"}); }, 5000);
+function runTests() {
+    var jqUnit = fluid.require("jqUnit");
+
+    // TODO:  Test adding/retrieving new content
+
+    // TODO:  Test updating content
+
+    // TODO:  Test viewing the history
+
+    // TODO:  Test viewing a diff between two revisions
+}
